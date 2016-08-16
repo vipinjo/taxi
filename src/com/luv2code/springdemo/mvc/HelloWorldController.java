7e201d746c6408c3a808e1c650521de20163fd6c
vipinjo <vipinjo@gmail.com>
2016-08-16 17:34:14 +1000

    initial commit src folder

---
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,43 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	// need a controller method to show the form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	// need a controller method to process the form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	// new controller method to read form data
+	// add data to the model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		// read the html parameters from request
+		String theName = request.getParameter("studentName");
+		
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		//create the message
+		String result = "Yo! " + theName;
+		
+		//add the message to model
+		model.addAttribute("message", result);
+		return "helloworld";
+	}
+
+}
